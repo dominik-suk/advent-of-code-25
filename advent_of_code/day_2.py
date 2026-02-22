36a620fcc49c1feac1aa37dfb891feea941f1760
dominik <dominik.sukic@wwb.de>
2026-02-22 20:47:00 +0100

    Day 2

--- advent_of_code/day_2.py
+++ advent_of_code/day_2.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from utils.io_actions import read_list
 
 from pathlib import Path
@@ -8,10 +10,10 @@ class DayTwo(Day):
         super().__init__(number=2)
 
     def solve_part_one(self, input_filepath: Path | str) -> int:
-        return get_password(input_filepath)
+        return get_password(input_filepath, id_is_repeated_twice)
 
     def solve_part_two(self, input_filepath: Path | str) -> int:
-        pass
+        return get_password(input_filepath, id_is_repeated_at_least_twice)
 
 
 def parse_segment(segment) -> tuple[int, int]:
@@ -21,12 +23,10 @@ def parse_segment(segment) -> tuple[int, int]:
     return first_id, last_id
 
 
-def id_is_valid(product_id: int) -> bool:
+def id_is_valid(product_id: int, found_sequence_func: Callable[[int], bool]) -> bool:
     if id_has_leading_zero(product_id):
         return False
-    if id_has_odd_length(product_id):
-        return True
-    if id_is_repeated_twice(product_id):
+    if found_sequence_func(product_id):
         return False
     return True
 
@@ -40,18 +40,42 @@ def id_has_leading_zero(product_id: int) -> bool:
 
 
 def id_is_repeated_twice(product_id: int) -> bool:
+    if id_has_odd_length(product_id):
+        return False
     product_id = str(product_id)
     first_half = product_id[:len(product_id) // 2]
     second_half = product_id[len(product_id) // 2:]
     return first_half == second_half
 
 
-def get_password(input_filepath: Path | str) -> int:
+def id_is_repeated_at_least_twice(product_id: int) -> bool:
+    product_id = str(product_id)
+    sequence = product_id[0]
+    for digit in product_id[1:]:
+        if digit == sequence[0] and sequence_is_repeating(product_id, sequence):
+            return True
+        sequence += digit
+    return False
+
+
+def sequence_is_repeating(product_id: str, sequence: str) -> bool:
+    found_sequence = True
+    for i in range(len(product_id)):
+        if len(product_id) % len(sequence) != 0:
+            found_sequence = False
+            break
+        if product_id[i] != sequence[i % len(sequence)]:
+            found_sequence = False
+            break
+    return found_sequence
+
+
+def get_password(input_filepath: Path | str, found_sequence_func: Callable[[int], bool]) -> int:
     segments = read_list(input_filepath)
     password = 0
     for segment in segments:
         first_id, last_id = parse_segment(segment)
         for product_id in range(first_id, last_id + 1):
-            if not id_is_valid(product_id):
+            if not id_is_valid(product_id, found_sequence_func):
                 password += product_id
     return password
--- main.py
+++ main.py
@@ -3,5 +3,4 @@ from advent_of_code import *
 
 if __name__ == "__main__":
     DayOne().run()
-    DayTwo().run_sample()
     DayTwo().run()
