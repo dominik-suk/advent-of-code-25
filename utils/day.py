d5d58c670582738103725e7b7ae2d2c0721d1387
dominik <dominik.sukic@wwb.de>
2026-03-02 11:11:44 +0100

    day

--- utils/day.py
+++ utils/day.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
+from email import message_from_string
 from pathlib import Path
+
+from utils.io_actions import measure_latency_of_function, measure_average_latency_of_function
 
 
 class Day(ABC):
@@ -26,3 +29,17 @@ class Day(ABC):
         print("Day {}".format(self.number))
         print("Part 1 Password: {}".format(self.solve_part_one(self.sample_filepath)))
         print("Part 2 Password: {}".format(self.solve_part_two(self.sample_filepath)), end="\n\n")
+
+    def run_and_measure_time(self):
+        print("Day {}".format(self.number))
+        password, time_taken = measure_latency_of_function(self.solve_part_one, self.input_filepath)
+        print("Part 1 Password: {}, Time taken: {} seconds".format(password, time_taken), end="\n\n")
+        password, time_taken = measure_latency_of_function(self.solve_part_two, self.input_filepath)
+        print("Part 2 Password: {}, Time taken: {} seconds".format(password, time_taken), end="\n\n")
+
+    def measure_average_time(self, n: int = 100):
+        print("Day {}".format(self.number))
+        time_taken = measure_average_latency_of_function(self.solve_part_one, self.input_filepath, n=n)
+        print("Time taken for part 1: {} seconds".format(time_taken), end="\n\n")
+        time_taken = measure_latency_of_function(self.solve_part_two, self.input_filepath)
+        print("Time taken for part 2: {} seconds".format(time_taken), end="\n\n")
--- utils/io_actions.py
+++ utils/io_actions.py
@@ -1,4 +1,6 @@
+import time
 from pathlib import Path
+from typing import Callable, Any
 
 
 def read_lines(path: Path | str) -> list[str]:
@@ -11,3 +13,23 @@ def read_list(path: Path | str) -> list[str]:
     if isinstance(path, str):
         path = Path(path)
     return path.read_text().split(',')
+
+
+def measure_latency_of_function(func: Callable, *args, **kwargs) -> tuple[Any, float]:
+    start_time = time.time()
+    result = func(*args, **kwargs)
+    end_time = time.time()
+    time_taken = round(end_time - start_time, 2)
+
+    return result, time_taken
+
+
+def measure_average_latency_of_function(func: Callable, *args, n=100, **kwargs) -> float:
+    sum_of_times = 0.0
+    for i in range(n):
+        start_time = time.time()
+        func(*args, **kwargs)
+        end_time = time.time()
+        time_taken = round(end_time - start_time, 2)
+        sum_of_times += time_taken
+    return round(sum_of_times / n, 2)
