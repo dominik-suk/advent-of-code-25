ed540ffc02cda196a2a34056feaaa0b947f6ec58
dominik <dominik.sukic@wwb.de>
2026-03-06 20:32:13 +0100

    Added placeholder days

---
+++ advent_of_code/day_11.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from utils.day import Day
+
+
+class DayEleven(Day):
+    def __init__(self):
+        super().__init__(number=11)
+
+    def solve_part_one(self, input_filepath: Path | str) -> int:
+        pass
+
+    def solve_part_two(self, input_filepath: Path | str) -> int:
+        pass
---
+++ advent_of_code/day_6.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from utils.day import Day
+
+
+class DaySix(Day):
+    def __init__(self):
+        super().__init__(number=6)
+
+    def solve_part_one(self, input_filepath: Path | str) -> int:
+        pass
+
+    def solve_part_two(self, input_filepath: Path | str) -> int:
+        pass
